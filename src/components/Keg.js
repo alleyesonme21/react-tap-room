e378aa684831a3e15959bf5a2afb36de11d10a22
jd <duverseaujohnny21@gmail.com>
2020-11-20 16:45:26 -0500

    add file and codes

---
+++ src/components/Keg.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Keg(props) {
+  return(
+    <React.Fragment>
+      <div onClick ={() => props.whenKegClicked(props.id)}>
+        <h3>keg unit</h3>
+        <h3>{props.name}</h3>
+        <h3>{props.brand}</h3>
+        <h3>{props.price}</h3>
+        <h3>{props.flavor}</h3>
+        <hr />
+
+      </div>
+    </React.Fragment>
+  )
+}
+Keg.propTypes = {
+  name: PropTypes.string,
+  brand: PropTypes.string,
+  price: PropTypes.number,
+  flavor: PropTypes.string,
+  id: PropTypes.number,
+  whenKegClicked: PropTypes.func
+}
+
+export default Keg;
