764c36cb2b1505183da70d260231c8a4111d90c6
jd <duverseaujohnny21@gmail.com>
2020-11-20 16:46:24 -0500

    add file and codes

---
+++ src/components/KegDetail.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function KegDetail(props) {
+  const {keg} = props;
+  return(
+    <React.Fragment>
+      <h1>Keg Detail</h1>
+      <h3>{keg.name}</h3>
+      <h3>{keg.brand}</h3>
+      <h3>{keg.price}</h3>
+      <h3>{keg.flavor}</h3>
+      <hr />
+    </React.Fragment>
+  );
+}
+KegDetail.propTypes ={
+  keg: PropTypes.object
+}
+export default KegDetail;
