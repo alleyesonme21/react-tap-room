84b2a66dc92de9e89c957e573a62388d8ad97c06
jd <duverseaujohnny21@gmail.com>
2020-11-20 16:47:14 -0500

    add file and codes

---
+++ src/components/NewKegForm.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import { v4 } from 'uuid';
+import PropTypes from 'prop-types';
+
+function NewKegForm(props) {
+  function handleNewKegFormSubmission(event) {
+    event.preventDefault();
+    props.onNewKegCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, flavor: event.target.flavor.value, id: v4()})
+
+  //   event.preventDefault();
+  //   console.log(event.target.name.value);
+  //   console.log(event.target.brand.value);
+  //   console.log(event.target.price.value);
+  //   console.log(event.target.flavor.value);
+  }
+
+  return (
+    <React.Fragment>
+      <form onSubmit={handleNewKegFormSubmission}>
+      <input
+      type='text'
+      name='name'
+      placeholder=' Names'/>
+      <input 
+      type='text'
+      name='brand'
+      placeholder='brand' />
+      <input 
+      type='text'
+      name='price'
+      placeholder='price' />
+      <input
+      type='text'
+      name='flavor'
+      placeholder='flavor'
+      />
+      <button type='submit'>Help</button>
+      </form>
+    </React.Fragment>
+  );
+}
+NewKegForm.propTypes = {
+  onNewKegCreation: PropTypes.func
+}
+
+
+export default NewKegForm;
