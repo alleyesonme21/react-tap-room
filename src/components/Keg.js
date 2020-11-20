64d8666f223b7f2271df420d3f16c365e8ca22bc
jd <duverseaujohnny21@gmail.com>
2020-11-20 18:21:39 -0500

    modified the h3

--- src/components/Keg.js
+++ src/components/Keg.js
@@ -6,12 +6,11 @@ function Keg(props) {
     <React.Fragment>
       <div onClick ={() => props.whenKegClicked(props.id)}>
         <h3>keg unit</h3>
-        <h3>{props.name}</h3>
-        <h3>{props.brand}</h3>
-        <h3>{props.price}</h3>
-        <h3>{props.flavor}</h3>
+        <h3>name:{props.name}</h3>
+        <h3>brand:{props.brand}</h3>
+        <h3>price:$ {props.price}</h3>
+        <h3>flavor:{props.flavor}</h3>
         <hr />
-
       </div>
     </React.Fragment>
   )
