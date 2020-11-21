36fa840403c36c9c29d6242ab3081923b666a743
jd <duverseaujohnny21@gmail.com>
2020-11-20 19:52:40 -0500

    add a semicolon

--- src/components/NewKegForm.js
+++ src/components/NewKegForm.js
@@ -5,13 +5,7 @@ import PropTypes from 'prop-types';
 function NewKegForm(props) {
   function handleNewKegFormSubmission(event) {
     event.preventDefault();
-    props.onNewKegCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, flavor: event.target.flavor.value, id: v4()})
-
-  //   event.preventDefault();
-  //   console.log(event.target.name.value);
-  //   console.log(event.target.brand.value);
-  //   console.log(event.target.price.value);
-  //   console.log(event.target.flavor.value);
+    props.onNewKegCreation({name: event.target.name.value, brand: event.target.brand.value, price: event.target.price.value, flavor: event.target.flavor.value, id: v4()});
   }
 
   return (
