e0a83be3d5d293eaec319dd4afcdcc0bb5721210
jd <duverseaujohnny21@gmail.com>
2020-11-29 13:22:34 -0500

    add all methods and conditions

--- src/components/KegControl.js
+++ src/components/KegControl.js
@@ -2,21 +2,42 @@ import React from 'react';
 import NewKegForm from './NewKegForm';
 import KegList from './KegList';
 import KegDetail from './KegDetail';
+import EditKegForm from './EditKegForm';
 
 class KegControl extends React.Component {
   constructor (props) {
     super(props)
     this.state = {
       formVisibleOnPage: false,
-      masterKegList: [], // new code
-      selectedKeg: null // new code for detail component
+      masterKegList: [],
+      selectedKeg: null,
+      editing: false
     };
-    this.handleClick = this.handleClick.bind(this); // new code here
   }
 
-  handleChangingSelectedKeg = (id) => {  // detail component                        
+
+  handleChangingSelectedKeg = (id) => {                         
     const selectedKeg = this.state.masterKegList.filter(keg => keg.id === id)[0];
     this.setState({selectedKeg: selectedKeg});
+  }
+
+  handleAddingNewKegToList = (newKeg) => {
+    const newMasterKegList = this.state.masterKegList.concat(newKeg);
+    this.setState({
+      masterKegList: newMasterKegList,
+      formVisibleOnPage: false});
+  }
+
+  handleSellUnit = (id) => {
+    const masterSellKegList = this.state.masterKegList;
+    masterSellKegList.map((keg) => {
+    if(keg.id === id && keg.numberLeft > 0){
+      keg.numberLeft = keg.numberLeft - 1;
+    }
+    return keg;
+    });
+    this.setState({masterKegList: masterSellKegList
+    });
   }
 
   handleClick = () => {
@@ -26,21 +47,50 @@ class KegControl extends React.Component {
         selectedKeg: null
       }); // detail part
     } else{ 
-    this.setState(prevState => ({
+    this.setState(prevState => ({ 
       formVisibleOnPage: !prevState.formVisibleOnPage
     }));
   }
 }
 
+handleDeletedKeg = (id) => {
+  const newMasterKegList = this.state.masterKegList.filter(keg => keg.id !== id);
+  this.setState({
+    masterKegList: newMasterKegList,
+    selectedKeg: null 
+  });
+}
+
+handleEditingKegList = (kegToEdit) => { 
+const editMasterKegList = this.state.masterKegList.filter(keg => keg.id !== this.state.selectedKeg.id).concat(kegToEdit);
+this.setState({
+  masterKegList: editMasterKegList,
+  editing: false,
+  selectedKeg: null
+  })
+}
+
+handleEditClick = () => {
+  this.setState({editing: true})
+}
+
+// handleDecrement = () => {
+//   this.state.masterKegList
+// }
+
   render() {
     let currentlyVisibleState = null;
     let buttonText = null; // new  code 
 
-    if (this.state.selectedKeg != null) {
-      currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg} /> //  detail part
-      buttonText = "return to Keg List";
+    if (this.state.editing) {
+      currentlyVisibleState = <EditKegForm keg = {this.state.selectedKeg} onEditKeg = {this.handleEditingKegList} />
+      buttonText = 'Return to ticket List';
     }
 
+    else if (this.state.selectedKeg != null) {
+      currentlyVisibleState = <KegDetail keg = {this.state.selectedKeg} onClickingSell={this.handleSellUnit} onClickingDelete= {this.handleDeletedKeg} onClickingEdit={this.handleEditClick} /> 
+      buttonText = "return to Keg List";
+    }
     else if(this.state.formVisibleOnPage) {
       currentlyVisibleState = <NewKegForm onNewKegCreation={this.handleAddingNewKegToList} />  // new code in this line
       buttonText = "Return to Keg List" //new code 
