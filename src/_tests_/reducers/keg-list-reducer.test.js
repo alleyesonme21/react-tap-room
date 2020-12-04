0db0bf6148adf0a965d7d38b85a6aba75f1d5f83
jd <duverseaujohnny21@gmail.com>
2020-12-04 14:07:44 -0500

    successfully failed

--- src/_tests_/reducers/keg-list-reducer.test.js
+++ src/_tests_/reducers/keg-list-reducer.test.js
@@ -1,18 +1,19 @@
 import kegListReducer from '../../reducers/keg-list-reducer';
 
 describe('kegListReducer', () => {
+ 
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(kegListReducer({}, {type: null})).toEqual({})
+  });
+
   let action;
   const kegData = {
     name: "Brana",
     brand: "Prestige",
-    price: "10",
+    price: 10,
     flavor: "Orange",
     id: 1
   }
-
-  test('Should return default state if there is no action type passed into the reducer', () => {
-    expect(kegListReducer({}, {type: null})).toEqual({})
-  });
   test('Should successfully and new keg data to masterKegList', () => {
     const { name, brand, price, flavor, id } = kegData;
     action = {
@@ -33,4 +34,31 @@ describe('kegListReducer', () => {
       }
     });
   });
+
+  const exState = {
+    1: {name: 'Brana',
+    brand: 'Prestige',
+    price: 10,
+    flavor: 'Orange',
+    id: 1 },
+    2: {name: 'JSEB',
+    brand: 'Bongou',
+    price: 20,
+    flavor: 'Fraise',
+    id: 2 }
+  }
+  test('Should successfully delete a keg', () => {
+    action = {
+      type: 'DELETE_KEG',
+      id: 2
+    }
+    expect(kegListReducer(exState, action)).toEqual({
+      1: {
+        name: 'Brana',
+        brand: 'Prestige',
+        price: 10,
+        flavor: 'Orange',
+        id: 1 }
+    })
+  })
 });
