65af2335df4c6fc329e0f92e4d4d93c37b01a940
jd <duverseaujohnny21@gmail.com>
2020-12-04 15:58:13 -0500

    second test successfully failed

--- src/_tests_/reducers/form-visible-reducer.test.js
+++ src/_tests_/reducers/form-visible-reducer.test.js
@@ -4,4 +4,8 @@ describe('formVisibleReducer', () => {
   test('Should return default state if no action type is recognized', () => {
     expect(formVisibleReducer(false, {type: null})).toEqual(false)
   })
+  test('Should TOGGLE visibility state to true', () => {
+    expect(formVisibleReducer(false, 'TOGGLE')).toEqual(true);
+
+  })
 })
