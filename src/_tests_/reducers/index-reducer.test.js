a619bac1c055fc5146ce5c3639b925085b1f1f49
jd <duverseaujohnny21@gmail.com>
2020-12-04 17:21:34 -0500

    import all files from index reducer

---
+++ src/_tests_/reducers/index-reducer.test.js
@@ -0,0 +1,7 @@
+import rootReducer from '../../reducers/index';
+import { createStore } from 'redux';
+import rootReducer from './reducers/index';
+import kegListReducer from '../../reducers/keg-list-reducer';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
+import selectedKegReducer from '../../reducers/selected-keg-reducer';
+import editKegReducer from '../../reducers/edit-keg-reducer';
