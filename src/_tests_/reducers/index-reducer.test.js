a05acc0bde8017bc61ac834741ec29003111677c
jd <duverseaujohnny21@gmail.com>
2020-12-04 18:32:50 -0500

    third  test passed

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,6 +1,8 @@
 import rootReducer from '../../reducers/index';
 import kegListReducer from '../../reducers/keg-list-reducer';
+import formVisibleReducer from '../../reducers/form-visible-reducer';
 import { createStore } from 'redux';
+
 
 let store = createStore(rootReducer);
 
@@ -15,5 +17,8 @@ describe('rootReducer', () => {
   })
   test('Check the initial state of kegListReducer matches root reducer', () => {
     expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, {type: null}));
+  })
+  test('Check that initial state of formVisibleReducer matches root reducer', () => {
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, {type: null}))
   })
 })
