70b0f3c2271a8b8643551e6278dc695f4e5ea433
jd <duverseaujohnny21@gmail.com>
2020-12-04 18:12:02 -0500

    first test passed

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -1,7 +1,15 @@
 import rootReducer from '../../reducers/index';
 import { createStore } from 'redux';
-import rootReducer from './reducers/index';
-import kegListReducer from '../../reducers/keg-list-reducer';
-import formVisibleReducer from '../../reducers/form-visible-reducer';
-import selectedKegReducer from '../../reducers/selected-keg-reducer';
-import editKegReducer from '../../reducers/edit-keg-reducer';
+
+let store = createStore(rootReducer);
+
+describe('rootReducer', () => {
+  test('Should return default state if no action type is recognized', () => {
+    expect(rootReducer({}, {type: null})).toEqual({
+      masterKegList: {},
+      formVisibleOnPage: false,
+      selectedKeg: null,
+      editing: false
+    })
+  })
+})
