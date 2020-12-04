e2dc9ea4d491fc8a7fda8586ea7e31a02eb18349
jd <duverseaujohnny21@gmail.com>
2020-12-04 16:13:42 -0500

    test  successfully failed

--- src/_tests_/reducers/edit-keg-reducer.test.js
+++ src/_tests_/reducers/edit-keg-reducer.test.js
@@ -4,4 +4,8 @@ describe('editKegReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(editKegReducer(false, {type: null})).toEqual(false)
   })
+  test('Should EDIT_FORM state to true', () => {
+    expect(editKegReducer(false, {type:'EDIT_FORM' })).toEqual(true);
+
+  })
 })
