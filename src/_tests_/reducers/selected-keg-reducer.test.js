a98cd65ecae2b7809a10e3fed6110d1ad6f7436b
jd <duverseaujohnny21@gmail.com>
2020-12-04 14:38:32 -0500

    successfully failed

---
+++ src/_tests_/reducers/selected-keg-reducer.test.js
@@ -0,0 +1,7 @@
+import selectedKegReducer from '../../reducers/selected-keg-reducer';
+
+describe('selectedKegReducer', () => {
+  test('Should return default state if there is no action type passed into the reducer', () => {
+    expect(selectedKegReducer(null, {type: null})).toEqual(false)
+  })
+})
