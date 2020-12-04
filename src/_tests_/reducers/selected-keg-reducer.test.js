2833e75be2d58c998d882f20e037f0fd6f2c32cc
jd <duverseaujohnny21@gmail.com>
2020-12-04 16:30:57 -0500

    test  successfully failed

--- src/_tests_/reducers/selected-keg-reducer.test.js
+++ src/_tests_/reducers/selected-keg-reducer.test.js
@@ -4,4 +4,7 @@ describe('selectedKegReducer', () => {
   test('Should return default state if there is no action type passed into the reducer', () => {
     expect(selectedKegReducer(null, {type: null})).toEqual(null)
   })
+  test('Should TOGGLE KEG to state to not null', () => {
+    expect(selectedKegReducer(null, {type: 'TOGGLE_KEG'})).toEqual(!null);
+  })
 })
