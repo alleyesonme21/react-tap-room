af7fdd2d16fe365254fabac3e50c39432521532d
jd <duverseaujohnny21@gmail.com>
2020-12-13 20:32:24 -0500

    add 4 tests all passed

--- src/_tests_/reducers/index-reducer.test.js
+++ src/_tests_/reducers/index-reducer.test.js
@@ -29,4 +29,40 @@ describe('rootReducer', () => {
   test('Check the initial state of editKegReducer matches the root reducer', () => {
     expect(store.getState().editing).toEqual(editKegReducer(undefined, {type: null}))
   })
+
+  test('Check that ADD_KEG action works for kegListReducer and root reducer', () => {
+    const action = {
+      type: 'ADD_KEG',
+      name: 'Brana',
+      brand: 'Prestige',
+      price: 10,
+      flavor: 'Orange',
+      id: 1
+    }
+    store.dispatch(action);
+    expect(store.getState().masterKegList).toEqual(kegListReducer(undefined, action));
+  });
+
+  test('Check that TOGGLE action works for formVisibleReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE'
+    }
+    store.dispatch(action);
+    expect(store.getState().formVisibleOnPage).toEqual(formVisibleReducer(undefined, action))
+  })
+  test('Check That TOGGLE_KEG action works for selectedKegReducer and root reducer', () => {
+    const action = {
+      type: 'TOGGLE_KEG'
+    }
+    store.dispatch(action);
+    expect(store.getState().selectedKeg).toEqual(selectedKegReducer(undefined, action));
+  })
+  test('Check that EDIT_FORM action works for editKegReducer and root reducer', () => {
+    const action = {
+      type: 'EDIT_KEG',
+    }
+    store.dispatch(action);
+    expect(store.getState().editing).toEqual(editKegReducer(undefined, action));
+  })
+  
 })
